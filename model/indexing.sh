92582d7a97f5c6ce87057c44cc6e564f7c657b5d
hanseokOh <hanseok.pro@gmail.com>
2023-12-08 14:28:36 +0900

    milvus indexing script

--- model/indexing.sh
+++ model/indexing.sh
@@ -10,9 +10,9 @@ PATH_TO_CORPUS=$DATA_PATH/특허Text_20230724_1.xlsx
 # PATH_TO_OUTPUT_DIR=./checkpoint/finetuned.contriever.summary2
 BSIZE=64 # origin:64
 
-# CKPT='facebook/mcontriever-msmarco' # bert-base-multilingual-cased
+CKPT='facebook/mcontriever-msmarco' # bert-base-multilingual-cased
 # CKPT='checkpoint/finetuned.contriever.summary2.w_negative.merge.query_origin.llm_gen.debug/checkpoint/step-1000/' # bert-base-multilingual-cased
-CKPT='checkpoint/finetuned.contriever.summary2.w_negative.merge.query_origin.llm_gen.debug/checkpoint/step-8000/' # bert-base-multilingual-cased
+# CKPT='checkpoint/finetuned.contriever.summary2.w_negative.merge.query_origin.llm_gen.debug/checkpoint/step-8000/' # bert-base-multilingual-cased
 
 #### For indexing & prediction
 # OUTPUT_DIR_NAME=./index/mcontriever-msmarco
@@ -25,10 +25,19 @@ OUTPUT_DIR_NAME=./index/tuned-8000
 echo "PATH to Index: ${PATH_TO_CORPUS}"
 echo "CKPT: ${CKPT}"
 
-CUDA_VISIBLE_DEVICES=0 python generate_passage_embeddings.py \
+# CUDA_VISIBLE_DEVICES=0 python generate_passage_embeddings.py \
+#     --model_name_or_path $CKPT \
+#     --output_dir ${OUTPUT_DIR_NAME}/embeddings/ \
+#     --passages ${PATH_TO_CORPUS} \
+#     --shard_id 0  \
+#     --num_shards 1 \
+#     --per_gpu_batch_size $BSIZE 
+    
+   
+CUDA_VISIBLE_DEVICES=0 python milvus_indexing.py \
     --model_name_or_path $CKPT \
-    --output_dir ${OUTPUT_DIR_NAME}/embeddings/ \
     --passages ${PATH_TO_CORPUS} \
     --shard_id 0  \
     --num_shards 1 \
     --per_gpu_batch_size $BSIZE 
+    #     --output_dir ${OUTPUT_DIR_NAME}/embeddings/ \
